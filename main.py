e9eb50de55f0ad40e5771adef1cfcd9f4c4a2f75
jayprajapati <jayprajapati2158@gmail.com>
2018-08-04 21:31:12 +0530

    v2

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
-import webbrowser,pyautogui
+import webbrowser,pyautogui,time
 
-i,j=pyautogui.position() #taking intial coordinate of mouse-pointer
+while 1:
 
-webbrowser.open_new_tab('http://www.google.com') # open extra-tab
+	i,j=pyautogui.position() #taking intial coordinate of mouse-pointer
 
-pyautogui.moveTo(90, 90) #mouse move to first tab
-pyautogui.click() 
-pyautogui.click() #giving control to browser
-pyautogui.press('f5')  #refresh page
-pyautogui.hotkey('ctrl', 'shift','tab') #move to last tab
-pyautogui.hotkey('ctrl', 'w') #close tab
-pyautogui.moveTo(1314, 41) #minimize window
-pyautogui.click() 
-pyautogui.moveTo(i,j) #ser pointer to intial coordianate
+	webbrowser.open_new_tab('http://www.google.com') # open extra-tab
+
+	pyautogui.moveTo(90, 90) #mouse move to first tab
+	pyautogui.click() 
+	pyautogui.click() #giving control to browser
+	pyautogui.press('f5')  #refresh page
+	pyautogui.hotkey('ctrl', 'shift','tab') #move to last tab
+	pyautogui.hotkey('ctrl', 'w') #close tab
+	pyautogui.moveTo(1314, 41) #minimize window
+	pyautogui.click() 
+	pyautogui.moveTo(i,j) #ser pointer to intial coordianate
+	time.sleep(5)
